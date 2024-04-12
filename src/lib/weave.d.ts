ac603fd7c89f675be153c4be715b3512b256a448
Håkon Matland <95701996+hakon-matland-adsk@users.noreply.github.com>
2024-04-12 13:44:18 +0200

    feat: add GeometryColorSelector (#9)
    * feat: add GeometryColorSelector
    
    This is a capability that potentially solves "Adjustable contrast of the shadow against the backdrop" in the "Suggestions for improvements" section of the README.
    
    It hopefully also makes the extension more useful for users like https://forums.autodesk.com/t5/forma-developer-forum/background-graphic-shadow-or-quot-shadow-study-extension-quot/m-p/12648176#M322 who struggles to see the contrast in their shadow studies.
    
    * fix: debouncedPersistedOnly

--- src/lib/weave.d.ts
+++ src/lib/weave.d.ts
@@ -1,23 +1,32 @@
-export declare module "preact/src/jsx" {
-  namespace JSXInternal {
-    interface IntrinsicElements {
-      "weave-button": JSX.HTMLAttributes<HTMLElement> & {
-        type?: "button" | "submit" | "reset";
-        variant?: "outlined" | "flat" | "solid";
-        density?: "high" | "medium";
-        iconposition?: "left" | "right";
-      };
-      "weave-select": JSX.HTMLAttributes<HTMLElement> & {
-        placeholder?: any;
-        value: any;
-        children: JSX.Element[];
-        onChange: (e: CustomEvent<{ value: string; text: string }>) => void;
-      };
-      "weave-select-option": JSX.HTMLAttributes<HTMLElement> & {
-        disabled?: true;
-        value: any;
-        children?: JSX.Element | string;
-      };
-    }
+namespace JSX {
+  interface IntrinsicElements {
+    "weave-button": JSX.HTMLAttributes<HTMLElement> & {
+      type?: "button" | "submit" | "reset";
+      variant?: "outlined" | "flat" | "solid";
+      density?: "high" | "medium";
+      iconposition?: "left" | "right";
+    };
+    "weave-select": JSX.HTMLAttributes<HTMLElement> & {
+      placeholder?: any;
+      value: any;
+      children: JSX.Element[];
+      onChange: (e: CustomEvent<{ value: string; text: string }>) => void;
+    };
+    "weave-select-option": JSX.HTMLAttributes<HTMLElement> & {
+      disabled?: true;
+      value: any;
+      children?: JSX.Element | string;
+    };
+    "weave-checkbox": {
+      onChange?: (e: CustomEvent) => void;
+      children?: JSX.Element | string;
+      style?: string;
+      checked: boolean;
+      showlabel?: boolean;
+      label?: string;
+      value?: string;
+      key?: string;
+      disabled?: boolean;
+    };
   }
 }
